82afd45acf8e6063a015ec8495639d16249f355c
Stone-Monkey1 <92111289+Stone-Monkey1@users.noreply.github.com>
2022-01-07 12:50:43 -0500

    Publishing

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -9,7 +9,6 @@ using Microsoft.Extensions.Logging;
 
 namespace API.Controllers
 {
-    [ServiceFilter(typeof(LogUserActivity))]
     [ApiController]
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
